3ee796cad7e7f76979c7d43cbd3265b0db5cd389
praneeth <praneethvenishetty@gmail.com>
2020-09-23 18:50:28 +0530

    Java Class Formatted

--- message_processor_notifications/src/main/java/message_processor_notifications/MuleComponentProcessListener.java
+++ message_processor_notifications/src/main/java/message_processor_notifications/MuleComponentProcessListener.java
@@ -21,10 +21,6 @@ public class MuleComponentProcessListener
 	public void onNotification(MessageProcessorNotification notification) {
 		EventContext process = notification.getEventContext();
 		String className = process.getClass().getSimpleName();
-		// boolean isOutbound = process instanceof DefaultHttpRequester;
-		// if (className.equalsIgnoreCase("DefaultHttpRequester") && isOutbound) {
-		// try {
-		// DefaultHttpRequester def = (DefaultHttpRequester) process;
 		if (notification.getAction().getActionId() == MessageProcessorNotification.MESSAGE_PROCESSOR_PRE_INVOKE) {
 			startTime = System.currentTimeMillis();
 			componentStartTimeHolder.put(process.toString(), startTime);
@@ -32,55 +28,12 @@ public class MuleComponentProcessListener
 		if (notification.getAction().getActionId() == MessageProcessorNotification.MESSAGE_PROCESSOR_POST_INVOKE) {
 			long executionTime = System.currentTimeMillis()
 					- (long) componentStartTimeHolder.put(process.toString(), startTime);
-			/*
-			 * log.warn("HTTP Request Call To : " + def.getHost() + " " + def.getPath() +
-			 * " took " + executionTime + "ms response time.");
-			 */
+
 			log.warn(computeDetails(notification.getComponent().getLocation().getParts()) + "\ncomponent took: "
 					+ executionTime + "ms response time.");
 		}
-		// } catch (Exception e) {
-	}
 
-	/*
-	 * } else if
-	 * (className.equalsIgnoreCase("SubflowInterceptingChainLifecycleWrapper")) {
-	 * SubflowInterceptingChainLifecycleWrapper subFlow =
-	 * (SubflowInterceptingChainLifecycleWrapper) process; if
-	 * (notification.getAction().getActionId() ==
-	 * MessageProcessorNotification.MESSAGE_PROCESSOR_PRE_INVOKE) { startTime =
-	 * System.currentTimeMillis();
-	 * componentStartTimeHolder.put(subFlow.getSubFlowName(), startTime); } if
-	 * (notification.getAction().getActionId() ==
-	 * MessageProcessorNotification.MESSAGE_PROCESSOR_POST_INVOKE) { long
-	 * executionTime = System.currentTimeMillis() - (long)
-	 * componentStartTimeHolder.get(subFlow.getSubFlowName());
-	 * log.warn("Sub-flow Processing Time: " + subFlow.getSubFlowName() + " took " +
-	 * executionTime + "ms response time."); } } else if
-	 * (className.equalsIgnoreCase("WeaveMessageProcessor")) { if
-	 * (notification.getAction().getActionId() ==
-	 * MessageProcessorNotification.MESSAGE_PROCESSOR_PRE_INVOKE) { startTime =
-	 * System.currentTimeMillis(); componentStartTimeHolder.put(process.toString(),
-	 * startTime); } if (notification.getAction().getActionId() ==
-	 * MessageProcessorNotification.MESSAGE_PROCESSOR_POST_INVOKE) { long
-	 * executionTime = System.currentTimeMillis() - (long)
-	 * componentStartTimeHolder.get(process.toString());
-	 * log.warn("Dataweave Processing Time: " + process.toString() + " took " +
-	 * executionTime + "ms response time."); } } else if
-	 * (className.equalsIgnoreCase("ScriptTransformer")) { if
-	 * (notification.getAction().getActionId() ==
-	 * MessageProcessorNotification.MESSAGE_PROCESSOR_PRE_INVOKE) { startTime =
-	 * System.currentTimeMillis(); componentStartTimeHolder.put(process.toString(),
-	 * startTime); } if (notification.getAction().getActionId() ==
-	 * MessageProcessorNotification.MESSAGE_PROCESSOR_POST_INVOKE) { long
-	 * executionTime = System.currentTimeMillis() - (long)
-	 * componentStartTimeHolder.get(process.toString());
-	 * log.warn("ScriptTransformer Processing Time: " + process.toString() +
-	 * " took " + executionTime + "ms response time.");
-	 * log.warn(computeDetails(notification.getComponent().getLocation().getParts())
-	 * + "\ncomponent took: " + executionTime + "ms response time."); } }
-	 */
-	// }
+	}
 
 	private StringBuilder computeDetails(List<LocationPart> parts) {
 		StringBuilder computeTime = new StringBuilder("");
